4f6d861f27c3c924c12a77bd5c2b5a949fb63c19
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-10-11 15:41:25 -0600

    Add realtime chat.

--- Main Menu.gd
+++ Main Menu.gd
@@ -4,7 +4,8 @@ onready var uuid = preload("uuid.gd").new()
 
 # The URL we will connect to
 # DEVELOPMENT VARIABLE
-export var websocket_url = "ws://miscreant-services-v1.herokuapp.com/"
+#export var websocket_url = "ws://miscreant-services-v1.herokuapp.com/"
+export var websocket_url = "ws://localhost:8080"
 
 # Our WebSocketClient instance
 var _client = WebSocketClient.new()
@@ -52,6 +53,8 @@ func _connected(proto = ""):
 	# sub-protocol (which is optional)
 	print("Connected with protocol: ", proto)
 	update_message_window("Sys: Connected to Server.")
+	get_node("ServerButton").text = "Connected."
+	ready = true
 	# You MUST always use get_peer(1).put_packet to send data to server,
 	# and not put_packet directly when not using the MultiplayerAPI.
 	var auth_string = "Auth "+id
@@ -63,8 +66,14 @@ func _on_data():
 	# using the MultiplayerAPI.
 	var data = _client.get_peer(1).get_packet().get_string_from_utf8()
 	print("Got data from server: ", data)
-	if "alive?" in data:
+	if data.begins_with("alive?"):
 		_client.get_peer(1).put_packet("yep".to_utf8())
+	elif data.begins_with("msg"):
+		# Repeat message to self.
+		update_message_window(data.trim_prefix("msg "))
+	
+
+	
 
 func _process(delta):
 	# Call this in _process or _physics_process. Data transfer, and signals
@@ -83,6 +92,10 @@ func _on_ServerButton_pressed():
 	var err = _client.connect_to_url(websocket_url)
 	if err != OK:
 		get_node("ServerButton").text = "Failed. Click to retry."
-	else:
-		get_node("ServerButton").text = "Connected."
-		ready = true
+
+
+func _on_SendMessageButton_pressed():
+	var content = "msg "+MessageEditor.text.strip_edges()
+	MessageEditor.text = ""
+	_client.get_peer(1).put_packet(content.to_utf8())
+
